f5655843ee8e7384c81ae6b493801ccfe9205d92
macstudent <fmsharmn@gmail.com>
2019-09-20 20:48:31 -0400

    current age

--- src/main/java/StringHandling.java
+++ src/main/java/StringHandling.java
@@ -1,3 +1,12 @@
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -6,9 +15,42 @@ public class StringHandling {
 
     public static void main(String[] args)
     {
+        /*DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
+*/
+
+        String s = "1994/12/20";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        Date d = null;
+        try {
+            d = sdf.parse(s);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Calendar c = Calendar.getInstance();
+        c.setTime(d);
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH) + 1;
+        int date = c.get(Calendar.DATE);
+        LocalDate l1 = LocalDate.of(year, month, date);
+        LocalDate now1 = LocalDate.now();
+        Period diff1 = Period.between(l1, now1);
+        System.out.println("age:" + diff1.getYears() + "years");
 
 
-         String s =String.format("%d $s $s", 10, "hello","world");
+
+
+
+       /* String pattern = "###,###,##,##";
+        DecimalFormat format = new DecimalFormat();
+        format.applyPattern(pattern);
+        int value = 12345678;
+        String output = format.format(value);
+        System.out.println(value + "  " +pattern + " " + output);
+
+
+        String s =String.format("%d $s $s", 10, "hello","world");
         System.out.println(s);
 
         System.out.print("Enter College name: ");
@@ -19,7 +61,7 @@ public class StringHandling {
 
 
 
-       /* *//*String s1 ="Hello";
+       *//* *//**//*String s1 ="Hello";
         String s2 ="Hello";
         String s3=null;
         String s4=new String("Hello World");
@@ -47,8 +89,8 @@ public class StringHandling {
         {
             System.out.println("s1!=s4");
         }
-*//*
-       *//* String s1="Hello";
+*//**//*
+       *//**//* String s1="Hello";
         System.out.println(s1);
 
        s1= s1.toLowerCase();
@@ -56,26 +98,26 @@ public class StringHandling {
 
        s1= s1.toUpperCase();
         System.out.println(s1);
-        *//*
+        *//**//*
        String collegeName = "Lambton College in Toronto";
 
-       *//*collegeName = String.valueOf(collegeName.length());
-       System.out.println(collegeName);*//*
+       *//**//*collegeName = String.valueOf(collegeName.length());
+       System.out.println(collegeName);*//**//*
 
-      *//* Boolean b =collegeName.contains("College");
-       System.out.println(b);*//*
+      *//**//* Boolean b =collegeName.contains("College");
+       System.out.println(b);*//**//*
 
 
-       *//*nt a[]= {1,2,3,4,5};
+       *//**//*nt a[]= {1,2,3,4,5};
 
        int b[]= new int[]{6,7,8,9,10};
 
        int c[]=new int[10];
 
         int c[];
-        *//*
+        *//**//*
 
-        *//*char c[] = collegeName.toCharArray();
+        *//**//*char c[] = collegeName.toCharArray();
         char rev[]=new char[c.length];
         int cnt=0;
         for(int i=c.length-1;i >=0; i--)
@@ -84,7 +126,7 @@ public class StringHandling {
         }
 
         String reverseString= new String(rev);
-        System.out.println("Reverse String = " + reverseString);*//*
+        System.out.println("Reverse String = " + reverseString);*//**//*
 
         String []strings= collegeName.split("");
         StringBuffer stringbuffer = new StringBuffer();
